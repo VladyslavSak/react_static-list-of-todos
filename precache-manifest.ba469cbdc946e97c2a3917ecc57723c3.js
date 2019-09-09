747cf17983bb416c444094e0b8969dbebcb7e19b
VladyslavSak <vladyslav.sak@gmail.com>
2019-09-09 22:13:57 +0300

    Updates

---
+++ precache-manifest.ba469cbdc946e97c2a3917ecc57723c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a73c5cb4f201771c0a3f5d779a1aa8b",
+    "url": "/react_static-list-of-todos/index.html"
+  },
+  {
+    "revision": "d09f4f00601c9beef798",
+    "url": "/react_static-list-of-todos/static/css/main.8cac8fc1.chunk.css"
+  },
+  {
+    "revision": "8d72c1bc687b008b68c1",
+    "url": "/react_static-list-of-todos/static/js/2.8047aade.chunk.js"
+  },
+  {
+    "revision": "d09f4f00601c9beef798",
+    "url": "/react_static-list-of-todos/static/js/main.1963de16.chunk.js"
+  },
+  {
+    "revision": "ea6f31786c2aed313835",
+    "url": "/react_static-list-of-todos/static/js/runtime~main.00529491.js"
+  }
+]);
